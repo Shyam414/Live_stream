6c02227129a992215e979fab728bdeead67cb8db
Shyam414 <sundarreddy414@gmail.com>
2026-08-17 22:50:42 +0530

    commit

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from ultralytics import YOLO
 
 app = Flask(__name__, static_folder="static")
 
-# Load pretrained model
 model = YOLO("yolo26n.pt")
 
 
@@ -18,23 +17,17 @@ def index():
 def detect():
     try:
         if "frame" not in request.files:
-            return jsonify({
-                "error": "No frame received"
-            }), 400
+            return jsonify({"error": "No frame received"}), 400
 
         file = request.files["frame"]
-
-        # Convert uploaded JPEG bytes to OpenCV frame
         image_bytes = file.read()
+
         np_array = np.frombuffer(image_bytes, np.uint8)
         frame = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
 
         if frame is None:
-            return jsonify({
-                "error": "Could not decode frame"
-            }), 400
+            return jsonify({"error": "Could not decode frame"}), 400
 
-        # YOLO detection
         results = model(frame, verbose=False)
 
         detections = []
@@ -42,7 +35,6 @@ def detect():
         for box in results[0].boxes:
             class_id = int(box.cls[0].item())
             confidence = float(box.conf[0].item())
-
             x1, y1, x2, y2 = box.xyxy[0].tolist()
 
             detections.append({
@@ -62,17 +54,7 @@ def detect():
 
     except Exception as e:
         print("ERROR:", e)
-
-        return jsonify({
-            "error": str(e)
-        }), 500
-
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=5000,
-        debug=True,
-        use_reloader=False,
-        
-    )
+    app.run(host="0.0.0.0", port=5000)
